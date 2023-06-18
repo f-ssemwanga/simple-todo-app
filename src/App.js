08017299331e6ed85f44e617f62c13b5a3b79ad1
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-18 17:08:23 +0100

    linked states and event listeners as well as event handlers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import { useState } from "react"
+import  "./styles.css"
+
+
 export const App = ()=>{
+   const [newItem, setNewItem] =useState("")
+   const [todos, setTodos] =useState([])
+
+   const handleUserInput = (event) =>{
+    //handles the state change
+    setNewItem(event.target.value)
+   }
+   const handleSubmit = (e) =>{
+    //handles the form sumit
+    e.preventDefault()
+    
+    setTodos(
+        //use a function here which returns the value in the todos array
+        currentTodos =>{
+            return [
+                ...currentTodos,
+                {id:crypto.randomUUID(),title:newItem, completed:false}
+            ]
+        }
+    )
+}
+console.log(todos)
     return(
-        <div>
-           Components go here
-        </div>
+        <>
+           <form 
+            onSubmit={handleSubmit} className="new-item-form" >
+            <div className="form-row">
+                <label htmlFor="item">New Item</label>
+                <input value ={newItem}
+                onChange ={handleUserInput}
+                type="text" id="item" />
+            </div>
+            <button className="btn">Add</button>
+           </form>
+        <h1 className ="header">To Do list</h1>
+
+        <ul className="list">
+            {/* Use a map function to loop through the to dos and render them */}
+            <li>
+                <label htmlFor="">
+                <input type="checkbox" />
+                Item 1
+                </label>
+                <button className ="btn btn-danger">Delete</button>
+            </li>
+
+        </ul>
+
+        </>
     )
 }
